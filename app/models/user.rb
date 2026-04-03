fa923ce62900018023007a7564cc3e06f0c73170
Yanyu Woo <wuyanyu0921@outlook.com>
2026-04-02 22:57:05 -0400

    feat: add password digest for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :prompt_versions, dependent: :destroy
 
+  has_secure_password
+
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+
+  private
+
+  def password_required?
+    new_record? || password.present?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_03_000527) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_03_120000) do
   create_table "prompt_versions", force: :cascade do |t|
     t.text "change_note"
     t.text "content"
@@ -48,6 +48,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_03_000527) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
+    t.string "password_digest"
     t.string "role"
     t.datetime "updated_at", null: false
     t.string "username"
