ab64edf190b6cb6688f7e3f86286d65a175b8c5e
Yanyu Woo <wuyanyu0921@outlook.com>
2026-04-02 21:25:40 -0400

    feat: add controller

--- app/controllers/api/v1/prompt_versions_controller.rb
+++ app/controllers/api/v1/prompt_versions_controller.rb
@@ -1,10 +1,30 @@
 class Api::V1::PromptVersionsController < ApplicationController
+  before_action :set_prompt
   def index
+    render json: @prompt.prompt_versions
   end
 
   def show
+    prompt_version = @prompt.prompt_versions.find(params[:id])
+    render json: prompt_version
   end
 
   def create
+    prompt_version = @prompt.prompt_versions.new(prompt_version_params)
+
+    if prompt_version.save
+      render json: prompt_version, status: :created
+    else
+      render json: {errors: prompt_version.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_prompt
+    @prompt = Prompt.find(params[:prompt_id])
+  end
+
+  def prompt_version_params
+    params.require(:prompt_version).permit(:version_number, :content, :change_note, :user_id)
   end
 end
--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::ReviewsController < ApplicationController
+  before_action :set_prompt
   def index
+    render json: @prompt.reviews
   end
 
   def create
+    review = @prompt.reviews.new(review_params)
+    if review.save
+      render json: review, status: :created
+    else
+      render json: {errors: review.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_prompt
+    @prompt = Prompt.find(params[:prompt_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :comment, :user_id)
   end
 end
